0bbd39a353b1d412613dfe9cbff086044ff50053
ganeshkarkuvel333-spec <ganesh.karkuvel333@gmail.com>
2026-08-11 17:03:57 +0530

    This is the latest codes for the Product Test, sorting ascending and descending.

--- src/test/java/testCases/LoginTest.java
+++ src/test/java/testCases/LoginTest.java
@@ -2,75 +2,97 @@ package testCases;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pages.LoginPage;
 import pages.ProductPage;
 import testComponents.BaseTest;
-import org.testng.asserts.SoftAssert;
-
 
 public class LoginTest extends BaseTest {
 
     @Test
     public void verifyValidLogin() {
+        log.info("Starting test: verifyValidLogin");
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.userLogin(readConfigUtils.getUsername(),readConfigUtils.getPassword());
+        loginPage.userLogin(readConfigUtils.getUsername(), readConfigUtils.getPassword());
+
         ProductPage productPage = new ProductPage(driver);
         String title = productPage.getValuePageTitle();
+
+        log.info("Validating page title after successful login");
         Assert.assertEquals(title, readConfigUtils.getTitle());
     }
 
     @Test
     public void verifyInValidLogin() {
+        log.info("Starting test: verifyInValidLogin");
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.userLogin(readConfigUtils.getInvalidUsername(),readConfigUtils.getPassword());
+        loginPage.userLogin(readConfigUtils.getInvalidUsername(), readConfigUtils.getPassword());
+
+        log.info("Validating error message for invalid username login attempt");
         Assert.assertEquals(loginPage.getErrorMessage(), readConfigUtils.getErrorMsgIncorrectCredentials());
     }
 
     @Test
     public void verifyLoginWithValidUserAndInvalidPassword() {
+        log.info("Starting test: verifyLoginWithValidUserAndInvalidPassword");
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.userLogin(readConfigUtils.getUsername(),readConfigUtils.getInvalidPassword());
+        loginPage.userLogin(readConfigUtils.getUsername(), readConfigUtils.getInvalidPassword());
+        log.info("Validating error message for invalid password login attempt");
         Assert.assertEquals(loginPage.getErrorMessage(), readConfigUtils.getErrorMsgIncorrectCredentials());
     }
 
     @Test
     public void verifyLoginWithInvalidUserAndInvalidPassword() {
+        log.info("Starting test: verifyLoginWithInvalidUserAndInvalidPassword");
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.userLogin(readConfigUtils.getInvalidUsername(),readConfigUtils.getInvalidPassword());
+        loginPage.userLogin(readConfigUtils.getInvalidUsername(), readConfigUtils.getInvalidPassword());
+
+        log.info("Validating error message for invalid username & password combination");
         Assert.assertEquals(loginPage.getErrorMessage(), readConfigUtils.getErrorMsgIncorrectCredentials());
     }
+
     @Test
-    public void verifyLoginWithEmptyUsernameAndPassword()
-    {
+    public void verifyLoginWithEmptyUsernameAndPassword() {
+        log.info("Starting test: verifyLoginWithEmptyUsernameAndPassword");
         driver.get(readConfigUtils.getUrl());
 
         LoginPage loginPage = new LoginPage(driver);
         loginPage.clickLogin();
+
+        log.info("Validating error message for empty credentials submission");
         Assert.assertEquals(loginPage.getErrorMessage(), readConfigUtils.getEmptyCredentialsError());
     }
 
     @Test
     public void verifyUsernameAndPasswordPlaceholders() {
-
+        log.info("Starting test: verifyUsernameAndPasswordPlaceholders");
         SoftAssert softAssert = new SoftAssert();
 
         driver.get(readConfigUtils.getUrl());
 
         LoginPage loginPage = new LoginPage(driver);
+        log.info("Checking username and password placeholder text");
         softAssert.assertEquals(loginPage.getUsernamePlaceholder(), readConfigUtils.getUsernamePlaceholder(), "Username placeholder mismatch");
         softAssert.assertEquals(loginPage.getPasswordPlaceholder(), readConfigUtils.getPasswordPlaceholder(), "Password placeholder mismatch");
         softAssert.assertAll();
     }
+
     @Test
-    public void verifyLogout(){
+    public void verifyLogout() {
+        log.info("Starting test: verifyLogout");
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.userLogin(readConfigUtils.getUsername(),readConfigUtils.getPassword());
+        loginPage.userLogin(readConfigUtils.getUsername(), readConfigUtils.getPassword());
+
         ProductPage productPage = new ProductPage(driver);
         String title = productPage.getValuePageTitle();
         Assert.assertEquals(title, readConfigUtils.getTitle());
+
+        log.info("Clicking hamburger menu and performing logout");
         productPage.clickHamburgerMenu();
         productPage.clickLogout();
+
         String currentUrl = driver.getCurrentUrl();
-        Assert.assertEquals(currentUrl,"https://www.saucedemo.com/","User was not logged out properly");
+        log.info("Validating URL after logout redirection");
+        Assert.assertEquals(currentUrl, "https://www.saucedemo.com/", "User was not logged out properly");
     }
 }
--- src/test/java/testComponents/BaseTest.java
+++ src/test/java/testComponents/BaseTest.java
@@ -1,27 +1,43 @@
 package testComponents;
 
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import utils.ReadConfigUtils;
 
+import java.time.Duration;
+
 public class BaseTest {
 
-    public  WebDriver driver;
+    public WebDriver driver;
     public ReadConfigUtils readConfigUtils;
+
+    // Initialize Log4j2 Logger
+    public static final Logger log = LogManager.getLogger(BaseTest.class);
 
     @BeforeMethod
     public void setup() {
+        log.info("Initializing ChromeDriver and configuring browser settings...");
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
+
         readConfigUtils = new ReadConfigUtils();
-        driver.get(readConfigUtils.getUrl());
+        String url = readConfigUtils.getUrl();
+
+        log.info("Navigating to URL: {}", url);
+        driver.get(url);
     }
+
     @AfterMethod
     public void teardown() {
-        driver.quit();
+        if (driver != null) {
+            log.info("Closing browser session...");
+            driver.quit();
+            log.info("Browser session closed successfully.");
+        }
     }
 }
