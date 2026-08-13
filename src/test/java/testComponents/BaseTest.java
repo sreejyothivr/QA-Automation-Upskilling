409036644bf601a5fc816cfbce2478db5425677f
shijinaayoob-source <shijinathanzeer@gmail.com>
2026-08-13 18:25:22 +0530

    Configure Chrome for Jenkins headless execution

--- src/test/java/testComponents/BaseTest.java
+++ src/test/java/testComponents/BaseTest.java
@@ -3,6 +3,7 @@ package testComponents;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import utils.ReadConfigUtils;
@@ -14,7 +15,14 @@ public class BaseTest {
 
     @BeforeMethod
     public void setup() {
-        driver = new ChromeDriver();
+       // driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(10));
         driver.manage().window().maximize();
         readConfigUtils = new ReadConfigUtils();
