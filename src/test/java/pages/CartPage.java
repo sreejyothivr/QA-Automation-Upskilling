4407a2739bd4df31ed942580edbd3a33279d8c0a
Amrutha Raj <amrutha.raj@qburst.com>
2026-09-09 00:22:59 +0530

    Add checkout test cases

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -3,16 +3,40 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.ArrayList;
+import java.time.Duration;
 import java.util.List;
 
 public class CartPage {
 
     private WebDriver driver;
+    private WebDriverWait wait;
+
+
+    private By cartItems = By.className("cart_item");
+    private By cartQuantity = By.className("cart_quantity");
+    private By checkoutButton = By.id("checkout");
 
     public CartPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
+    public List<WebElement> getCartItems() {
+
+        return wait.until(
+                ExpectedConditions.visibilityOfAllElementsLocatedBy(cartItems)
+        );
+    }
+
+    public String getProductQuantity(WebElement cartItem) {
+
+        return cartItem.findElement(cartQuantity).getText();
+    }
+
+    public void clickCheckout() {
+        wait.until(ExpectedConditions.elementToBeClickable(checkoutButton)).click();
+    }
 }
